5675582aabfa4edc25900c7502462d8a3bea842c
ArabDeveloper <arabdevelopers731@gmail.com>
2025-02-13 10:20:42 +0200

    create invite modal

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,158 +1,192 @@
-    import { v } from "convex/values";
-    import { query,mutation } from "./_generated/server";
+import { v } from "convex/values";
+import { query,mutation } from "./_generated/server";
 import { auth } from "./auth";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
 const generateCode = () => {
-    const code = Array.from(
-        {
-            length:6
-        },
-        ()=>{
-            "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random()*36)]
-        }
-    ).join("");
-    return code
+const code = Array.from(
+    {
+        length:6
+    },
+    ()=>"0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random()*36)]
+).join("");
+return code
 }
-    export const createWorkspace = mutation({
-    args: {
-        name: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx); 
 
-        if (!userId) {
-        throw new Error("User must be authenticated to create a workspace.");
+export const createNewJoinCode = mutation({
+args:{
+    workspaceId:v.id("workSpaces"),
+
+},
+
+handler:async (ctx,args)=>{
+
+    console.log("start")
+    const userId = await getAuthUserId(ctx); 
+    
+    if (!userId) {
+    throw new Error("User must be authenticated to create a Join code.");
+    }
+
+    const member  = await ctx.db.query("member")
+    .withIndex("by_workspace_user_id",(q) =>
+            q.eq("workspaceId",args.workspaceId).eq("userId",userId)).unique();
+
+    if(!member || member.role !== "admin"){
+        throw new Error("User must be admin to update Join code");
+    }
+
+    const joinCode = generateCode();
+
+    
+
+    await ctx.db.patch(args.workspaceId,{joinCode})
+
+    return args.workspaceId
+}
+
+
+})
+
+export const createWorkspace = mutation({
+args: {
+    name: v.string(),
+},
+handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx); 
+
+    if (!userId) {
+    throw new Error("User must be authenticated to create a workspace.");
+    }
+
+    const joinCode =generateCode();
+    
+    const workspaceId = await ctx.db.insert("workSpaces", {
+    name : args.name,
+    userId,
+    joinCode,
+    });
+
+    // create member 
+    await ctx.db.insert("member",{
+        userId, 
+        workspaceId,
+        role:"admin"
+    })
+
+    // create channel
+    await ctx.db.insert("channel",{
+        name:"general",
+        workspaceId
+    })
+
+    return workspaceId;
+},
+});
+
+
+export const getWorkSpaces = query({
+    args: {},
+    handler: async (ctx) => {
+        const userId = await getAuthUserId(ctx);
+        if(!userId){    
+            return [];
         }
 
-        const joinCode =generateCode();
-        
-        const workspaceId = await ctx.db.insert("workSpaces", {
-        name : args.name,
-        userId,
-        joinCode,
-        });
+        const members= await ctx.db.query("member").withIndex("by_user_id",(q) => q.eq("userId",userId)).collect();
 
-        // create member 
-        await ctx.db.insert("member",{
-            userId,
-            workspaceId,
-            role:"admin"
-        })
-
-        // create channel
-        await ctx.db.insert("channel",{
-            name:"general",
-            workspaceId
-        })
-
-        return workspaceId;
+        const workspaceIds = members.map((member) => member.workspaceId);
+        const workspaces= []
+        for(const workspaceId of workspaceIds){
+            const workspace = await ctx.db.get(workspaceId);
+            if(workspace){
+                workspaces.push(workspace);
+            }
+        }
+        return workspaces
     },
-    });
-
-
-    export const getWorkSpaces = query({
-        args: {},
-        handler: async (ctx) => {
-            const userId = await getAuthUserId(ctx);
-            if(!userId){    
-                return [];
-            }
-
-            const members= await ctx.db.query("member").withIndex("by_user_id",(q) => q.eq("userId",userId)).collect();
-
-            const workspaceIds = members.map((member) => member.workspaceId);
-            const workspaces= []
-            for(const workspaceId of workspaceIds){
-                const workspace = await ctx.db.get(workspaceId);
-                if(workspace){
-                    workspaces.push(workspace);
-                }
-            }
-            return workspaces
-        },
-    });
+});
 
 export const getWorkspace = query({
-    args :{id: v.id("workSpaces")},
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx);
+args :{id: v.id("workSpaces")},
+handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
 
-        if(!userId){
-            throw new Error("User must be authenticated ");
-        }
-
-        const member  = await ctx.db.query("member")
-        .withIndex("by_workspace_user_id",(q) =>
-                q.eq("workspaceId",args.id).eq("userId",userId)).unique();
-
-        if(!member){
-            return null
-        }
-        
-        return await ctx.db.get(args.id);
-
+    if(!userId){
+        throw new Error("User must be authenticated ");
     }
+
+    const member  = await ctx.db.query("member")
+    .withIndex("by_workspace_user_id",(q) =>
+            q.eq("workspaceId",args.id).eq("userId",userId)).unique();
+
+    if(!member){
+        return null
+    }
+    
+    return await ctx.db.get(args.id);
+
+}
 });
 
 export const updateWorkspace = mutation({
-    args: {
-        id: v.id("workSpaces"),
-        name: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx);
+args: {
+    id: v.id("workSpaces"),
+    name: v.string(),
+},
+handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
 
-        if(!userId){
-            throw new Error("User must be authenticated ");
-        }
+    if(!userId){
+        throw new Error("User must be authenticated ");
+    }
 
-        const member  = await ctx.db.query("member")
-        .withIndex("by_workspace_user_id",(q) =>
-                q.eq("workspaceId",args.id).eq("userId",userId)).unique();
+    const member  = await ctx.db.query("member")
+    .withIndex("by_workspace_user_id",(q) =>
+            q.eq("workspaceId",args.id).eq("userId",userId)).unique();
 
-        if(!member || member.role !== "admin"){
-            throw new Error("User must be admin to update workspace");
-        }
+    if(!member || member.role !== "admin"){
+        throw new Error("User must be admin to update workspace");
+    }
 
-        await ctx.db.patch(args.id, {
-            name: args.name,
-        });
+    await ctx.db.patch(args.id, {
+        name: args.name,
+    });
 
-        return args.id;
-    },
+    return args.id;
+},
 });
 export const removeWorkspace = mutation({
-    args: {
-        id: v.id("workSpaces"),
+args: {
+    id: v.id("workSpaces"),
 
-    },
-    handler: async (ctx, args) => {
-        const userId = await getAuthUserId(ctx);
+},
+handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
 
-        if(!userId){
-            throw new Error("User must be authenticated ");
-        }
+    if(!userId){
+        throw new Error("User must be authenticated ");
+    }
 
-        const member  = await ctx.db.query("member")
-        .withIndex("by_workspace_user_id",(q) =>
-                q.eq("workspaceId",args.id).eq("userId",userId)).unique();
+    const member  = await ctx.db.query("member")
+    .withIndex("by_workspace_user_id",(q) =>
+            q.eq("workspaceId",args.id).eq("userId",userId)).unique();
 
-        if(!member || member.role !== "admin"){
-            throw new Error("User must be admin to update workspace");
-        }
+    if(!member || member.role !== "admin"){
+        throw new Error("User must be admin to update workspace");
+    }
 
-        const [members] =await Promise.all([
-            ctx.db.query("member").withIndex("by_workspace_id",(q)=>q.eq("workspaceId",args.id)).collect(),
-            
-        ])
+    const [members] =await Promise.all([
+        ctx.db.query("member").withIndex("by_workspace_id",(q)=>q.eq("workspaceId",args.id)).collect(),
+        
+    ])
 
-        for(const member of members){
-            await ctx.db.delete(member._id);
-        }
+    for(const member of members){
+        await ctx.db.delete(member._id);
+    }
 
-        await ctx.db.delete(args.id);
+    await ctx.db.delete(args.id);
 
-        return args.id;
-    },
+    return args.id;
+},
 });
--- src/app/workspaces/[workspaceId]/workspace-components/InviteModal.tsx
+++ src/app/workspaces/[workspaceId]/workspace-components/InviteModal.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { useCreateNewJoinCode } from '@/components/workspaces/api/use-create-new-join-code'
 import { useGetWorkspaceId } from '@/components/workspaces/hooks/use-get-workspace-id'
+import { useConfirm } from '@/hooks/use-confirm'
 import { CheckIcon, CopyIcon } from 'lucide-react'
 import React, { use, useState } from 'react'
 import { toast } from 'sonner'
@@ -14,6 +16,13 @@ interface InviteModalProps {
 const InviteModal = ({ open, setOpen, name, joinCode }: InviteModalProps) => {
 
     const workspaceId = useGetWorkspaceId()
+
+    const [ConfirmDialogUI, confirm] = useConfirm(
+        "Are You Sure ",
+        "Are you sure you want to generate a new invite code?"
+    )
+
+    const { mutate, isPending } = useCreateNewJoinCode()
 
     const [isCopy, setIsCopy] = useState(false)
 
@@ -33,43 +42,83 @@ const InviteModal = ({ open, setOpen, name, joinCode }: InviteModalProps) => {
         }, 3000)
 
     }
+
+    const handleCloseModal = () => {
+        setOpen(false)
+    }
+
+    const handleGenerateNewCode = async () => {
+
+        const ok = await confirm()
+        if (!ok) return
+        mutate({ workspaceId }, {
+            onSuccess: () => {
+                toast.success("new code generated")
+            },
+            onError: () => {
+                toast.error("error generating new code")
+            }
+        })
+    }
+
+    console.log(open)
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger>Invite People</DialogTrigger>
+        <>
+            <ConfirmDialogUI />
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger>Invite People</DialogTrigger>
 
-            {/*start  dialog content  */}
+                {/*start  dialog content  */}
 
-            <DialogContent>
+                <DialogContent>
 
-                {/* start dialog header  */}
+                    {/* start dialog header  */}
 
-                <DialogHeader>
+                    <DialogHeader>
 
-                    <DialogTitle>Invite people to {name}</DialogTitle>
+                        <DialogTitle>Invite people to {name}</DialogTitle>
 
-                    <DialogDescription>Copy the code below and send it to the person you want to invite</DialogDescription>
+                        <DialogDescription>Copy the code below and send it to the person you want to invite</DialogDescription>
 
-                </DialogHeader>
+                    </DialogHeader>
+                    {/* start display join code  */}
+                    <div className="flex items-center justify-center font-bold text-5xl uppercase">
+                        <span>{joinCode}</span>
+                        <Button
+                            variant={null}
+                            onClick={handleCopy}
+                        >
+                            {
+                                isCopy ? <CheckIcon className=' text-green-600' /> : <CopyIcon />
 
-                <div className="flex items-center justify-center font-bold text-5xl uppercase">
-                    <span>{joinCode}</span>
-                    <Button
-                        variant={null}
-                        onClick={handleCopy}
-                    >
-                        {
-                            isCopy ? <CheckIcon className=' text-green-600' /> : <CopyIcon />
+                            }
+                        </Button>
+                    </div>
+                    {/* end display join code  */}
 
-                        }
-                    </Button>
-                </div>
+                    {/* start display generate new code  */}
+                    <div className="flex items-center justify-between">
+                        <Button variant={'outline'}
+                            onClick={handleGenerateNewCode}
+                        >
+                            Generate new code
+                        </Button>
+
+                        <Button onClick={handleCloseModal}>
+                            Close
+                        </Button>
+
+                    </div>
+                    {/* end  display generate new code  */}
 
 
-                {/* end  dialog header  */}
-            </DialogContent>
 
-            {/*end   dialog content  */}
-        </Dialog>
+                    {/* end  dialog header  */}
+                </DialogContent>
+
+                {/*end   dialog content  */}
+            </Dialog>
+        </>
     )
 }
 
